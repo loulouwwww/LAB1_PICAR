96a1918fbcd0624792a16ac850d819f7f4bf0cdd
SKY <skymac@KYMacBook-Pro.local>
2022-02-11 20:00:20 -0600

    bug1

--- picar_4wd/__init__.py
+++ picar_4wd/__init__.py
@@ -3,31 +3,36 @@ from picar_4wd.adc import ADC
 from picar_4wd.pin import Pin
 from picar_4wd.motor import Motor
 from picar_4wd.servo import Servo
-from picar_4wd.ultrasonic import Ultrasonic 
+from picar_4wd.ultrasonic import Ultrasonic
 from picar_4wd.speed import Speed
-from picar_4wd.filedb import FileDB  
+from picar_4wd.filedb import FileDB
 from picar_4wd.utils import *
 import time
 
 # Config File:
 config = FileDB("picar-4wd-config")
-left_front_reverse = config.get('left_front_reverse', default_value = False)
-right_front_reverse = config.get('right_front_reverse', default_value = False)
-left_rear_reverse = config.get('left_rear_reverse', default_value = False)
-right_rear_reverse = config.get('right_rear_reverse', default_value = False)    
+left_front_reverse = config.get('left_front_reverse', default_value=False)
+right_front_reverse = config.get('right_front_reverse', default_value=False)
+left_rear_reverse = config.get('left_rear_reverse', default_value=False)
+right_rear_reverse = config.get('right_rear_reverse', default_value=False)
 
-ultrasonic_servo_offset = int(config.get('ultrasonic_servo_offset', default_value = 0)) 
+ultrasonic_servo_offset = int(config.get(
+    'ultrasonic_servo_offset', default_value=0))
 
 # Init motors
-left_front = Motor(PWM("P13"), Pin("D4"), is_reversed=left_front_reverse) # motor 1
-right_front = Motor(PWM("P12"), Pin("D5"), is_reversed=right_front_reverse) # motor 2
-left_rear = Motor(PWM("P8"), Pin("D11"), is_reversed=left_rear_reverse) # motor 3
-right_rear = Motor(PWM("P9"), Pin("D15"), is_reversed=right_rear_reverse) # motor 4
+left_front = Motor(PWM("P13"), Pin(
+    "D4"), is_reversed=left_front_reverse)  # motor 1
+right_front = Motor(PWM("P12"), Pin(
+    "D5"), is_reversed=right_front_reverse)  # motor 2
+left_rear = Motor(PWM("P8"), Pin("D11"),
+                  is_reversed=left_rear_reverse)  # motor 3
+right_rear = Motor(PWM("P9"), Pin(
+    "D15"), is_reversed=right_rear_reverse)  # motor 4
 
 # left_front_speed = Speed(12)
 # right_front_speed = Speed(16)
 left_rear_speed = Speed(25)
-right_rear_speed = Speed(4)  
+right_rear_speed = Speed(4)
 
 # Init Greyscale
 gs0 = ADC('A5')
@@ -42,6 +47,7 @@ us = Ultrasonic(Pin('D8'), Pin('D9'))
 
 servo = Servo(PWM("P0"), offset=ultrasonic_servo_offset)
 
+
 def start_speed_thread():
     # left_front_speed.start()
     # right_front_speed.start()
@@ -49,7 +55,9 @@ def start_speed_thread():
     right_rear_speed.start()
 
 ##################################################################
-# Grayscale 
+# Grayscale
+
+
 def get_grayscale_list():
     adc_value_list = []
     adc_value_list.append(gs0.read())
@@ -57,36 +65,40 @@ def get_grayscale_list():
     adc_value_list.append(gs2.read())
     return adc_value_list
 
+
 def is_on_edge(ref, gs_list):
     ref = int(ref)
-    if gs_list[2] <= ref or gs_list[1] <= ref or gs_list[0] <= ref:  
+    if gs_list[2] <= ref or gs_list[1] <= ref or gs_list[0] <= ref:
         return True
     else:
         return False
 
-def get_line_status(ref,fl_list):#170<x<300
+
+def get_line_status(ref, fl_list):  # 170<x<300
     ref = int(ref)
     if fl_list[1] <= ref:
         return 0
-    
+
     elif fl_list[0] <= ref:
         return -1
 
     elif fl_list[2] <= ref:
         return 1
 
+
 ########################################################
 # Ultrasonic
 ANGLE_RANGE = 180
 STEP = 18
 us_step = STEP
-angle_distance = [0,0]
+angle_distance = [0, 0]
 current_angle = 0
 max_angle = ANGLE_RANGE/2
 min_angle = -ANGLE_RANGE/2
 scan_list = []
 
 errors = []
+
 
 def run_command(cmd=""):
     import subprocess
@@ -111,13 +123,15 @@ def do(msg="", cmd=""):
         errors.append("%s error:\n  Status:%s\n  Error:%s" %
                       (msg, status, result))
 
+
 def get_distance_at(angle):
     global angle_distance
     servo.set_angle(angle)
-    time.sleep(0.04)
+    time.sleep(0.1)
     distance = us.get_distance()
     angle_distance = [angle, distance]
     return distance
+
 
 def get_status_at(angle, ref1=35, ref2=10):
     dist = get_distance_at(angle)
@@ -128,6 +142,7 @@ def get_status_at(angle, ref1=35, ref2=10):
     else:
         return 0
 
+
 def scan_step(ref):
     global scan_list, current_angle, us_step
     current_angle += us_step
@@ -137,7 +152,7 @@ def scan_step(ref):
     elif current_angle <= min_angle:
         current_angle = min_angle
         us_step = STEP
-    status = get_status_at(current_angle, ref1=ref)#ref1
+    status = get_status_at(current_angle, ref1=ref)  # ref1
 
     scan_list.append(status)
     if current_angle == min_angle or current_angle == max_angle:
@@ -153,11 +168,14 @@ def scan_step(ref):
 
 ########################################################
 # Motors
+
+
 def forward(power):
     left_front.set_power(power)
     left_rear.set_power(power)
     right_front.set_power(power)
     right_rear.set_power(power)
+
 
 def backward(power):
     left_front.set_power(-power)
@@ -165,11 +183,13 @@ def backward(power):
     right_front.set_power(-power)
     right_rear.set_power(-power)
 
+
 def turn_left(power):
     left_front.set_power(-power)
     left_rear.set_power(-power)
     right_front.set_power(power)
     right_rear.set_power(power)
+
 
 def turn_right(power):
     left_front.set_power(power)
@@ -177,11 +197,13 @@ def turn_right(power):
     right_front.set_power(-power)
     right_rear.set_power(-power)
 
+
 def stop():
     left_front.set_power(0)
     left_rear.set_power(0)
     right_front.set_power(0)
     right_rear.set_power(0)
+
 
 def set_motor_power(motor, power):
     if motor == 1:
@@ -205,10 +227,12 @@ def set_motor_power(motor, power):
 #     elif arg[0] == 4:
 #         return right_rear_speed()
 
+
 def speed_val():
     return (left_rear_speed() + right_rear_speed()) / 2.0
 
-######################################################## 
+
+########################################################
 if __name__ == '__main__':
     start_speed_thread()
     while 1:
