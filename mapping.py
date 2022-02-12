d2ab96cf1bb7393f73389a089ea7a9be5977c4ee
SKY <skymac@KYMacBook-Pro.local>
2022-02-11 21:22:43 -0600

    2

--- mapping.py
+++ mapping.py
@@ -284,7 +284,7 @@ def detect():
     return
 
 
-def set_target(rel_y=40, rel_x=0):  # relative position(cm) to car
+def set_target(rel_y=40, rel_x=40):  # relative position(cm) to car
     global curr_status, global_map, target_y, target_x
     curr_status = 0
     y = int(rel_y/unit)+curr_y
