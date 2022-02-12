b97891020ef06fe31a7f67aa0c1a60193fdd2be7
hainal2 <hainal2@illinois.edu>
2022-02-12 21:37:26 +0000

    part1

---
+++ step3.py
@@ -0,0 +1,46 @@
+import picar_4wd as fc
+from random import randint
+import helper_functions as hf
+import time
+speed = 30
+
+
+def main():
+    thr = 10
+    while True:
+        scan_list = fc.scan_step(35)
+        if not scan_list:
+            continue
+
+        tmp = scan_list[3:7]
+        print(tmp)
+        i = randint(1, 2)
+        if tmp != [2,2,2,2]:
+            fc.stop()
+            time.sleep(0.3)
+            fc.backward(speed/10)
+            time.sleep(0.3)
+                
+            if i == 1:
+               hf.turn_left_deg()
+               time.sleep(1)
+               fc.forward(speed)
+               time.sleep(1)
+               hf.turn_right_deg()
+               time.sleep(1)
+            else:
+                hf.turn_right_deg()
+                time.sleep(1)
+                fc.forward(speed)
+                time.sleep(1)
+                hf.turn_left_deg()
+                time.sleep(1)
+        
+        fc.forward(speed)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        fc.stop()
