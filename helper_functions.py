cc2a511b96306c8a6360efa89838846217c673ea
SKY <skymac@KYMacBook-Pro.local>
2022-02-11 21:30:33 -0600

    2

--- helper_functions.py
+++ helper_functions.py
@@ -7,47 +7,43 @@ import math
 
 
 def turn_right_deg():
-    speedd = Speed(30)
-    speedd.start()
+
     fc.turn_right(100)
     for i in range(11):
         time.sleep(0.05)
-    speedd.deinit()
     fc.stop()
+    time.sleep(0.1)
 
 
 def turn_left_deg():
-    speedd = Speed(30)
-    speedd.start()
     fc.turn_left(100)
     for i in range(10):
         time.sleep(0.05)
-    speedd.deinit()
     fc.stop()
+    time.sleep(0.1)
 
 # 50mm*2grid
 
 # n means grid
+
+
 def forward_grid(n=1):
-    speed4 = Speed(30)
-    speed4.start()
     fc.forward(100)
     for i in range(n):
         time.sleep(0.1)
-    speed4.deinit()
     fc.stop()
+    time.sleep(0.1)
 
 
 def backward_grid(n=1):
-    speed4 = Speed(30)
-    speed4.start()
     fc.backward(100)
     for i in range(n):
         time.sleep(0.1)
-    speed4.deinit()
     fc.stop()
+    time.sleep(0.1)
+
 
 if __name__ == "__main__":
-    #forward_grid(2)
+    # forward_grid(2)
     backward_grid(2)
     fc.stop()
